73fc6a9ba5bd9d9226b7ed917721048d96618b95
Dan Brendza <dan.brendza01@student.csulb.edu>
2026-02-19 17:40:49 -0800

    ex2

--- js/ex1.js
+++ js/ex1.js
@@ -2,16 +2,20 @@
 Exercise 1 JavaScript code
 */
 
+// Collect user input
 let userNum = prompt("Enter a number between 1-100: ");
 
+// Checks if input is a number
 if(isNaN(userNum)) {
   throw ("Sorry, " + userNum + " is not a valid entry.");
 }
 
+// Checks if input is in range
 else if (userNum >= 1 && userNum <=100) {
   console.log("Thank you! You entered " + userNum + ", a valid number.");
 }
 
+// Output for invalid entries
 else {
   throw ("Sorry, " + userNum + " is not a valid entry.");
 }
--- js/ex2.js
+++ js/ex2.js
@@ -1,5 +1,21 @@
 /* Tutorial 4
-Exercise JavaScript code
+Exercise 2 JavaScript code
 */
 
-console.log("Brendza's Output from Tutorial 4 Exercise 2");
+// Initalize arrays
+let array1 = [4, 8, 12, 5, 20];
+let array2 = ["a", 2, "Hello", 6, [4, 5], "bye", {name: "Alice", score: 98}];
+
+// Function to create object with array properites
+function arrayProp(data) {
+  let arrayObject = {
+    firstElement: data[0],
+    lastElement: data[data.length - 1],
+    length: data.length
+  };
+  return arrayObject;
+}
+
+// Test cases
+console.log(arrayProp(array1));
+console.log(arrayProp(array2));
