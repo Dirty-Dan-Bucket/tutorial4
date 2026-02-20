6e3b92a0f1b25f31afbfa2c5ca337b543661c601
Dan Brendza <dan.brendza01@student.csulb.edu>
2026-02-19 17:23:18 -0800

    ex1

--- tutorial4/js/ex1.js
+++ tutorial4/js/ex1.js
@@ -2,4 +2,16 @@
 Exercise 1 JavaScript code
 */
 
-console.log("Brendza's Output from Tutorial 4 Exercise 1");
+let userNum = prompt("Enter a number between 1-100: ");
+
+if(isNaN(userNum)) {
+  throw ("Sorry, " + userNum + " is not a valid entry.");
+}
+
+else if (userNum >= 1 && userNum <=100) {
+  console.log("Thank you! You entered " + userNum + ", a valid number.");
+}
+
+else {
+  throw ("Sorry, " + userNum + " is not a valid entry.");
+}
