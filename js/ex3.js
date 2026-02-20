fcd2927da4f2396842f30b721b0e0a2c0b597bed
Dan Brendza <dan.brendza01@student.csulb.edu>
2026-02-19 18:29:26 -0800

    comments

--- js/ex3.js
+++ js/ex3.js
@@ -14,16 +14,21 @@ let guessNum = randomInt(1, 100);
 let userNum = 0;
 let attempts = 0;
 
+// Loops until correct quess or user types q
 while(userNum != "q") {
+  // Asks user for input and adds attempts
   userNum = prompt("Guess the number (enter q to quit): ");
   attempts += 1;
+  // Win condition
   if (userNum === guessNum) {
     console.log("Correct! It took you " + attempts + " attempts to guess the correct number.");
     break;
   }
+  // Guess too low
   else if (userNum < guessNum) {
     console.log("Too low, guess again");
   }
+  // Guess too high
   else if (userNum > guessNum) {
     console.log("Too high, guess again");
   }
